406948b236ded84f57b43e7a3c4fc57e95ea98ba
Amkam <amaduskamara@gmail.com>
2023-07-09 13:07:44 +0000

    Add styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>{new Date().toLocaleTimeString()}. We're currently open</div>;
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  // if (hour >= openHour && hour <= closeHour) alert("We're currently open");
+  // else alert("Sorry, we're closed!");
+
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}. We're currently open
+    </footer>
+  );
 };
 
 export default Footer;
