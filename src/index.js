e6c12b48139a8a2382becfeb9a183a6bd257fab2
Amkam <amaduskamara@gmail.com>
2023-07-09 13:08:06 +0000

    Add styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -8,4 +9,4 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
