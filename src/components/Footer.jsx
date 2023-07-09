f8608e689c283b08a98194f8c52321bac59346bf
Amkam <amaduskamara@gmail.com>
2023-07-09 11:15:06 +0000

    Add date display with date function

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <div>{new Date().toLocaleTimeString()}. We're currently open</div>;
 };
 
 export default Footer;
