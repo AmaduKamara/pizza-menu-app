8684754516e136bd1e994b512086b7f2969e3493
Amkam <amaduskamara@gmail.com>
2023-07-09 13:07:34 +0000

    Add styles

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Menu from "./components/Menu";
 
-
 const App = () => {
   return (
-    <>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
-    </>
+    </div>
   );
 };
 
