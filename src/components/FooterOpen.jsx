0b0179310a62c572efcf3472aed8e621d38c9b16
Amkam <amaduskamara@gmail.com>
2023-07-10 08:50:47 +0000

    Create FooterOpen.jsx

---
+++ src/components/FooterOpen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FooterOpen = ({closeHour}) => {
+  return (
+    <div className="order">
+      <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+      <button className="btn">Order</button>
+    </div>
+  );
+}
+
+export default FooterOpen
