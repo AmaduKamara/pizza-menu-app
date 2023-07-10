6bf314d7fbbecbf99384f20c17945c958c0e7c76
Amkam <amaduskamara@gmail.com>
2023-07-10 08:58:41 +0000

    Update FooterOpen.jsx

--- src/components/FooterOpen.jsx
+++ src/components/FooterOpen.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const FooterOpen = ({closeHour}) => {
+const FooterOpen = ({ openHour, closeHour }) => {
   return (
     <div className="order">
-      <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+      <p>
+        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+        online.
+      </p>
       <button className="btn">Order</button>
     </div>
   );
-}
+};
 
-export default FooterOpen
+export default FooterOpen;
