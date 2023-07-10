bab96bc4ca95b75206ce55a1d4ceca7ff25d946b
Amkam <amaduskamara@gmail.com>
2023-07-10 08:50:50 +0000

    Update Pizza.jsx

--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Pizza = ({ pizza }) => {
-  const { photoName, name, ingredients, price } = pizza;
+  const { photoName, name, ingredients, price, soldOut } = pizza;
+
+  if (soldOut) {
+    return (
+      <li className="pizza sold-out">
+        <img src={photoName} alt="" />
+        <div>
+          <h3>{name}</h3>
+          <p>{ingredients}</p>
+          <p>{price}</p>
+        </div>
+      </li>
+    );
+  }
+
   return (
     <li className="pizza">
       <img src={photoName} alt="" />
