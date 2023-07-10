357b6e8e320911b7b0fa5733aeaf88a481e8bc3e
Amkam <amaduskamara@gmail.com>
2023-07-10 08:27:39 +0000

    Use ternary operator to conditionally render jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,18 +4,21 @@ import Pizza from "./Pizza";
 import pizzaData from "../data";
 
 const Menu = () => {
-  const pizzas = pizzaData
+  const pizzas = pizzaData;
+  // const pizzas = [];
   const numPizzas = pizzas.length;
 
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizza={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later:) </p>
       )}
     </main>
   );
